a440629def47df2371b8709fbfb0fe58e5b209a4
Fabio Mascarenhas <mascarenhas@gmail.com>
2016-06-16 07:43:11 -0700

    remove table allocations in resume and wrap

--- src/taggedcoro.lua
+++ src/taggedcoro.lua
@@ -27,26 +27,32 @@ function M.yield(tag, ...)
   return yield(tag, ...)
 end
 
+local function resumekk(co, meta, ...)
+  meta.stacked = false
+  return M.resume(co, ...)
+end
+
+local function resumek(co, meta, ok, tag, ...)
+  tagset[meta.tag] = tagset[meta.tag] - 1
+  if not ok then return false, tag end
+  if status(co) == "dead" then
+    return ok, tag, ...
+  end
+  tag = tag or DEFAULT_TAG
+  if meta.tag == tag then
+    return true, ...
+  else
+    if not isyieldable() then return error("no coroutine for tag " .. tostring(tag)) end
+    meta.stacked = true
+    return resumekk(co, meta, yield(tag, ...))
+  end
+end
+
 function M.resume(co, ...)
   local meta = coros[co]
   if meta.stacked then return error("cannot resume stacked coroutine") end
   tagset[meta.tag] = (tagset[meta.tag] or 0) + 1
-  local res = { resume(co, ...) }
-  tagset[meta.tag] = tagset[meta.tag] - 1
-  if not res[1] then return false, res[2] end
-  if status(co) == "dead" then
-    return table.unpack(res)
-  end
-  local tag = res[2] or DEFAULT_TAG
-  if meta.tag == tag then
-    return true, table.unpack(res, 3)
-  else
-    if not isyieldable() then return error("no coroutine for tag " .. tostring(tag)) end
-    meta.stacked = true
-    local res = { yield(tag, table.unpack(res, 3)) }
-    meta.stacked = false
-    return M.resume(co, table.unpack(res))
-  end
+  return resumek(co, meta, resume(co, ...))
 end
 
 function M.status(co)
@@ -64,16 +70,19 @@ function M.isyieldable(tag)
   return tagset[tag] and (tagset[tag] > 0)
 end
 
+local function wrapk(ok, err, ...)
+  if ok then
+    return err, ...
+  else
+    return error(err)
+  end
+end
+
 function M.wrap(f, tag)
   tag = tag or DEFAULT_TAG
   local co = M.create(f, tag)
   return function (...)
-           local res = { M.resume(co, ...) }
-           if res[1] then
-             return table.unpack(res, 2)
-           else
-             return error(res[2])
-           end
+           return wrapk(M.resume(co, ...))
          end
 end
 
